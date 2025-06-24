f56c35684d3fdac3095cd8c50a81e2793f98eccc
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-06-24 13:36:01 -0400

    refactor: use gill createTransaction

--- templates/template-next-tailwind-basic/src/components/solana/use-wallet-transaction-sign-and-send.tsx
+++ templates/template-next-tailwind-basic/src/components/solana/use-wallet-transaction-sign-and-send.tsx
@@ -1,32 +1,21 @@
 import { useWalletUi } from '@wallet-ui/react'
-import {
-  appendTransactionMessageInstruction,
-  assertIsTransactionMessageWithSingleSendingSigner,
-  createTransactionMessage,
-  getBase58Decoder,
-  IInstruction,
-  pipe,
-  setTransactionMessageFeePayerSigner,
-  setTransactionMessageLifetimeUsingBlockhash,
-  signAndSendTransactionMessageWithSigners,
-  TransactionSendingSigner,
-} from 'gill'
+import type { IInstruction, TransactionSendingSigner } from 'gill'
+import { createTransaction, getBase58Decoder, signAndSendTransactionMessageWithSigners } from 'gill'
 
 export function useWalletTransactionSignAndSend() {
   const { client } = useWalletUi()
 
   return async (ix: IInstruction, signer: TransactionSendingSigner) => {
     const { value: latestBlockhash } = await client.rpc.getLatestBlockhash().send()
-    const message = pipe(
-      createTransactionMessage({ version: 0 }),
-      (tx) => setTransactionMessageFeePayerSigner(signer, tx),
-      (tx) => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
-      (tx) => appendTransactionMessageInstruction(ix, tx),
-    )
 
-    assertIsTransactionMessageWithSingleSendingSigner(message)
+    const transaction = createTransaction({
+      feePayer: signer,
+      version: 0,
+      latestBlockhash,
+      instructions: [ix],
+    })
 
-    const signature = await signAndSendTransactionMessageWithSigners(message)
+    const signature = await signAndSendTransactionMessageWithSigners(transaction)
 
     return getBase58Decoder().decode(signature)
   }
