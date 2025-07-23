6af474e0e4fc0319530c6e7fbb5416b457ec6086
r_mayyank <thelueblue@gmail.com>
2025-07-24 04:25:05 +0530

    add anchor e2e tests to gill- templates

--- templates/template-next-tailwind-basic/anchor/tests/basic.test.ts
+++ templates/template-next-tailwind-basic/anchor/tests/basic.test.ts
@@ -1,7 +1,24 @@
+import { getGreetInstruction, getGreetInstructionDataDecoder, GREET_DISCRIMINATOR } from "../src/client/js"
+import * as programClient from "../src/client/js";
+
+import { createSolanaClient, createTransaction, generateKeyPairSigner, getSignatureFromTransaction, signTransactionMessageWithSigners } from "gill"
+
 describe('basic', () => {
-  // TODO: Implement tests for the basic program based on the Codama generated client.
-  // Use tests in `legacy/legacy-next-tailwind-basic/anchor/tests/basic.test.ts` as a reference.
+  const { rpc } = createSolanaClient({ urlOrMoniker: 'localnet' })
   it('should run the program and print "GM!" to the transaction log', async () => {
-    expect(true).toBe(true)
+    const greetInstruction = getGreetInstruction()
+    const signer = await generateKeyPairSigner();
+    const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+    const tx = createTransaction({
+      feePayer: signer,
+      instructions: [greetInstruction],
+      version: "legacy",
+      latestBlockhash
+    })
+    const signedTransaction = await signTransactionMessageWithSigners(tx)
+    let sig = getSignatureFromTransaction(signedTransaction);
+    console.log("Transaction signature:", sig);
   })
 })
+
+
