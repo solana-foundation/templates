1c4c64bb50f81a788bde9bd5ee64f538c2175d06
r_mayyank <thelueblue@gmail.com>
2025-07-24 04:25:05 +0530

    test: add anchor e2e tests to gill- templates

--- templates/template-next-tailwind-counter/anchor/tests/counter.test.ts
+++ templates/template-next-tailwind-counter/anchor/tests/counter.test.ts
@@ -1,27 +1,134 @@
+// import { getCloseInstruction, getDecrementInstruction, getIncrementInstruction, getSetInstruction } from "../src/client/js"
+import { createSolanaClient, createTransaction, generateKeyPairSigner, KeyPairSigner, signTransactionMessageWithSigners } from "gill"
+import * as program from "../src/client/js"
+
 describe('counter', () => {
-  // TODO: Implement tests for the counter program based on the Codama generated client.
-  // Use tests in `legacy/legacy-next-tailwind-counter/anchor/tests/counter.test.ts` as a reference.
-  it.skip('Initialize Counter', async () => {
-    expect(true).toBe(true)
+  const { rpc, sendAndConfirmTransaction } = createSolanaClient({ urlOrMoniker: 'localnet' })
+  let payer: KeyPairSigner
+  let counter: KeyPairSigner
+  let value                   // TODO: add this type
+
+  beforeAll(async () => {
+    payer = await generateKeyPairSigner();
+    counter = await generateKeyPairSigner();
+    value = await rpc.getLatestBlockhash().send();  // We may have a bug here
+  })
+
+  it('Initialize Counter', async () => {
+
+    const initializeInstruction = program.getInitializeInstruction({
+      payer: payer,
+      counter: counter
+    })
+    const tx = createTransaction({
+      feePayer: payer,
+      instructions: [initializeInstruction],
+      version: "legacy",
+      latestBlockhash: value
+    })
+    const signedTransaction = await signTransactionMessageWithSigners(tx);
+    await sendAndConfirmTransaction(signedTransaction)
+    const currentCounter = await program.fetchCounter(
+      rpc,
+      counter.address
+    )
+    expect(currentCounter.data.count).toEqual(0)
   })
 
   it.skip('Increment Counter', async () => {
-    expect(true).toBe(true)
+    const incrementInstruction = program.getIncrementInstruction({
+      counter: counter.address
+    })
+    const tx = createTransaction({
+      feePayer: payer,
+      instructions: [incrementInstruction],
+      version: "legacy",
+      latestBlockhash: value
+    })
+    const signedTransaction = await signTransactionMessageWithSigners(tx)
+    await sendAndConfirmTransaction(signedTransaction)
+    const currentCount = await program.fetchCounter(
+      rpc,
+      counter.address
+    )
+    expect(currentCount.data.count).toEqual(1)
   })
 
   it.skip('Increment Counter Again', async () => {
-    expect(true).toBe(true)
+    const incrementInstruction = program.getIncrementInstruction({
+      counter: counter.address
+    })
+    const tx = createTransaction({
+      feePayer: payer,
+      instructions: [incrementInstruction],
+      version: "legacy",
+      latestBlockhash: value
+    })
+    const signedTransaction = await signTransactionMessageWithSigners(tx)
+    await sendAndConfirmTransaction(signedTransaction)
+    const { data } = await program.fetchCounter(
+      rpc,
+      counter.address
+    )
+    expect(data.count).toEqual(2)
   })
 
   it.skip('Decrement Counter', async () => {
-    expect(true).toBe(true)
+    const decrementInstruction = program.getDecrementInstruction({
+      counter: counter.address
+    })
+    const tx = createTransaction({
+      feePayer: payer,
+      instructions: [decrementInstruction],
+      version: "legacy",
+      latestBlockhash: value
+    })
+    const signedTransaction = await signTransactionMessageWithSigners(tx)
+    await sendAndConfirmTransaction(signedTransaction)
+    const currentCount = await program.fetchCounter(
+      rpc,
+      counter.address
+    )
+    expect(currentCount.data.count).toEqual(1)
   })
 
   it.skip('Set counter value', async () => {
-    expect(true).toBe(true)
+    const setInstruction = program.getSetInstruction({
+      counter: counter.address,
+      value: 42
+    })
+    const tx = createTransaction({
+      feePayer: payer,
+      instructions: [setInstruction],
+      version: "legacy",
+      latestBlockhash: value
+    })
+    const signedTransaction = await signTransactionMessageWithSigners(tx)
+    await sendAndConfirmTransaction(signedTransaction)
+    const currentCount = await program.fetchCounter(
+      rpc,
+      counter.address
+    )
+    expect(currentCount.data.count).toEqual(42)
   })
 
   it.skip('Set close the counter account', async () => {
-    expect(true).toBe(true)
+    const closeInsturction = program.getCloseInstruction({
+      payer: payer,
+      counter: counter.address
+    })
+    const tx = createTransaction({
+      feePayer: payer,
+      instructions: [closeInsturction],
+      version: "legacy",
+      latestBlockhash: value
+    })
+    const signedTransaction = await signTransactionMessageWithSigners(tx)
+    await sendAndConfirmTransaction(signedTransaction)
+    const currentCount = await program.fetchCounter(
+      rpc,
+      counter.address
+    ) 
+    expect(currentCount).toBeNull()
   })
 })
