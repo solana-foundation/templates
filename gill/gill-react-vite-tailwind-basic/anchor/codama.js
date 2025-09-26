b286614cb0b0e3b798408dec336f1abd6c3d8bb3
Bram Borggreve <beeman@beeman.nl>
2025-09-26 20:01:04 +0300

    chore: update to gill to 0.1.1

--- gill/gill-react-vite-tailwind-basic/anchor/codama.js
+++ gill/gill-react-vite-tailwind-basic/anchor/codama.js
@@ -1,4 +1,6 @@
-import { createCodamaConfig } from 'gill'
+// NOTE: The local createCodamaConfig is a temporary workaround until gill ships the fix for https://github.com/gillsdk/gill/issues/207
+// Future versions can "import { createCodamaConfig } from 'gill'" directly
+import { createCodamaConfig } from './src/create-codama-config.js'
 
 export default createCodamaConfig({
   clientJs: 'anchor/src/client/js/generated',
