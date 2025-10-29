3caaea5d33b911afa848f1e4a655f44ac6e966ac
guibibeau <g.bibeaulaviolette@gmail.com>
2025-10-28 21:41:48 -0400

    fix: update vite configs for vitest

--- gill/gill-react-vite-tailwind-counter/vite.config.ts
+++ gill/gill-react-vite-tailwind-counter/vite.config.ts
@@ -1,22 +1,20 @@
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, type PluginOption } from 'vite'
 import { resolve } from 'node:path'
+
+const plugins: PluginOption[] = [
+  nodePolyfills({}) as PluginOption,
+  react() as PluginOption,
+  tailwindcss() as PluginOption,
+  viteTsconfigPaths({
+    root: resolve(__dirname),
+  }) as PluginOption,
+]
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    nodePolyfills({}),
-    react(),
-    tailwindcss(),
-    viteTsconfigPaths({
-      //
-      root: resolve(__dirname),
-    }),
-  ],
-  test: {
-    globals: true,
-  },
+  plugins,
 })
