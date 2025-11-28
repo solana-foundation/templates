6faf8877f97091b527d1d4989aa357b68d68d101
beeman <beeman@beeman.nl>
2025-11-28 11:50:16 +0400

    feat: add web3js-expo-minimal template

---
+++ mobile/web3js-expo-minimal/features/account/use-account-get-balance.tsx
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query'
+import { PublicKey } from '@solana/web3.js'
+import { useMobileWalletAdapter } from '@wallet-ui/react-native-web3js'
+
+export function useAccountGetBalance({ publicKey }: { publicKey: PublicKey }) {
+  const { connection, clusterId } = useMobileWalletAdapter()
+  return useQuery({
+    queryKey: ['get-balance', clusterId, publicKey.toString()],
+    queryFn: () => connection.getBalance(publicKey),
+  })
+}
