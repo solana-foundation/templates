f71e3e26655d5ab3120d8ee1b03aebfb3a896ed8
guibibeau <g.bibeaulaviolette@gmail.com>
2025-12-17 17:35:31 +0400

    chore: remove test folder

--- my-solana-app/my-solana-app.config.ts
+++
@@ -1,7 +0,0 @@
-import react from "@my-solana-appjs/plugin-react";
-import tailwindcss from "@tailwindcss/my-solana-app";
-import { defineConfig } from "my-solana-app";
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
--- my-solana-app/src/my-solana-app-env.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference types="my-solana-app/client" />
