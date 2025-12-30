bc9833a6a489c2909f54932d19c42604710f7218
beeman <beeman@beeman.nl>
2025-12-30 23:02:04 +0700

    chore(mobile): update templates to `@wallet-ui/react-native-web3js@2.2.0` (#248)
    * chore(mobile): update dependencies
    
    * refactor(mobile): move web3js-expo to @wallet-ui/react-native-web3js
    
    * refactor(mobile): move web3js-expo-paper to @wallet-ui/react-native-web3js

--- mobile/web3js-expo-paper/components/app-providers.tsx
+++ mobile/web3js-expo-paper/components/app-providers.tsx
@@ -1,12 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MobileWalletProvider } from '@wallet-ui/react-native-web3js'
 import { PropsWithChildren } from 'react'
-import { ClusterProvider } from './cluster/cluster-provider'
-import { SolanaProvider } from '@/components/solana/solana-provider'
-import { AppTheme } from '@/components/app-theme'
 import { AuthProvider } from '@/components/auth/auth-provider'
+import { ClusterProvider, useCluster } from '@/components/cluster/cluster-provider'
+import { AppTheme } from '@/components/app-theme'
 
 const queryClient = new QueryClient()
-
 export function AppProviders({ children }: PropsWithChildren) {
   return (
     <AppTheme>
@@ -18,5 +17,20 @@ export function AppProviders({ children }: PropsWithChildren) {
         </ClusterProvider>
       </QueryClientProvider>
     </AppTheme>
+  )
+}
+
+// We have this SolanaProvider because of the network switching logic.
+// If you only connect to a single network, use MobileWalletProvider directly.
+function SolanaProvider({ children }: PropsWithChildren) {
+  const { selectedCluster } = useCluster()
+  return (
+    <MobileWalletProvider
+      chain={selectedCluster.id}
+      endpoint={selectedCluster.endpoint}
+      identity={{ name: 'Wallet UI Example Web3js Expo' }}
+    >
+      {children}
+    </MobileWalletProvider>
   )
 }
--- mobile/web3js-expo-paper/components/cluster/cluster-ui-genesis-hash.tsx
+++ mobile/web3js-expo-paper/components/cluster/cluster-ui-genesis-hash.tsx
@@ -1,12 +1,12 @@
-import { useConnection } from '@/components/solana/solana-provider'
 import { useQuery } from '@tanstack/react-query'
+import { useMobileWallet } from '@wallet-ui/react-native-web3js'
 import { AppText } from '@/components/app-text'
 import React from 'react'
 import { ellipsify } from '@/utils/ellipsify'
 import { Cluster } from '@/components/cluster/cluster'
 
 export function ClusterUiGenesisHash({ selectedCluster }: { selectedCluster: Cluster }) {
-  const connection = useConnection()
+  const { connection } = useMobileWallet()
   const query = useQuery({
     queryKey: ['get-genesis-hash', { selectedCluster }],
     queryFn: () => connection.getGenesisHash(),
