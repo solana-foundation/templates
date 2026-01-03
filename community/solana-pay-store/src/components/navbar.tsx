615215cdc712ce8f2136ddf39fc5b8e26e0469c3
cxalem <smarketing.cuz@gmail.com>
2026-01-03 13:34:00 +0100

    removing unused import

--- community/solana-pay-store/src/components/navbar.tsx
+++ community/solana-pay-store/src/components/navbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import Image from 'next/image'
 import { ShoppingCart, Receipt } from 'lucide-react'
 import { ThemeSelect } from '@/components/theme-select'
 import { Button } from '@/components/ui/button'
