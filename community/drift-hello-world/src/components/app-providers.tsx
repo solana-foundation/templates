424ce734b54b833fb79edecfe49fc5b921927b53
rajgoesout <rajgoesout@gmail.com>
2026-01-06 17:02:43 +0530

    feat: Add community template for Drift hello world
    Closes https://github.com/solana-foundation/templates/issues/153

---
+++ community/drift-hello-world/src/components/app-providers.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import React from 'react'
+import { SolanaProvider } from '@/components/solana/solana-provider'
+
+export function AppProviders({ children }: Readonly<{ children: React.ReactNode }>) {
+  return <SolanaProvider>{children}</SolanaProvider>
+}
